53d91bcb45782b3eec443298f4d622dac839f132
takatoh <takatoh.m@gmail.com>
2019-07-31 14:15:45 +0900

    main.go: Use indexmaker package.

--- main.go
+++ main.go
@@ -1,36 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
+//	"fmt"
+//	"os"
+//	"path/filepath"
+
+	"github.com/takatoh/mkdocindex/indexmaker"
 )
 
 
 func main() {
-	var files []string
-	var directories []string
+//	var files []string
+//	var directories []string
 
 	homeDir := "."
 
-	entries, _ := filepath.Glob(homeDir + "/*")
-
-	for _, f := range entries {
-		finfo, _ := os.Stat(f)
-		if finfo.IsDir() {
-			directories = append(directories, f)
-		} else {
-			files = append(files, f)
-		}
-	}
-
-	fmt.Println("Directories:")
-	for _, d := range directories {
-		fmt.Println(d)
-	}
-	fmt.Println("")
-	fmt.Println("Files:")
-	for _, f := range files {
-		fmt.Println(f)
-	}
+	maker := indexmaker.New(homeDir)
+	maker.Make()
 }

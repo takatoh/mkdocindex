833cdb8c19f775c18e09d309d812841beee9dcd2
takatoh <takatoh.m@gmail.com>
2019-07-31 14:02:14 +0900

    main.go: Separate entries to directory or file.

--- main.go
+++ main.go
@@ -2,17 +2,35 @@ package main
 
 import (
 	"fmt"
-//	"os"
+	"os"
 	"path/filepath"
 )
 
 
 func main() {
+	var files []string
+	var directories []string
+
 	homeDir := "."
 
 	entries, _ := filepath.Glob(homeDir + "/*")
 
 	for _, f := range entries {
+		finfo, _ := os.Stat(f)
+		if finfo.IsDir() {
+			directories = append(directories, f)
+		} else {
+			files = append(files, f)
+		}
+	}
+
+	fmt.Println("Directories:")
+	for _, d := range directories {
+		fmt.Println(d)
+	}
+	fmt.Println("")
+	fmt.Println("Files:")
+	for _, f := range files {
 		fmt.Println(f)
 	}
 }

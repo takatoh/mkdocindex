ffb6b87e38a083e313914ea0ddbb2d5414966e65
takatoh <takatoh.m@gmail.com>
2019-08-01 13:02:00 +0900

    -v option to show version.

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
 	"path/filepath"
 
 	"github.com/takatoh/mkdocindex/indexmaker"
 )
 
 
+const (
+	progVersion = "v0.1.0"
+)
+
+
 func main() {
+	opt_version := flag.Bool("v", false, "Show version.")
+	flag.Parse()
+
+	if *opt_version {
+		fmt.Println(progVersion)
+		os.Exit(0)
+	}
+
 	homeDir, _ := filepath.Abs(".")
 
 	maker := indexmaker.New(homeDir)

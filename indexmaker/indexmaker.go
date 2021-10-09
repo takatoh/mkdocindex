f9bb533b04b6c0a6ab7fe0ef64496442e62c72c4
takatoh <takatoh.m@gmail.com>
2021-10-10 06:11:43 +0900

    indexmaker/indexmaker.go: Remove unnecessary constant.

--- indexmaker/indexmaker.go
+++ indexmaker/indexmaker.go
@@ -9,42 +9,6 @@ import (
 	"github.com/takatoh/mkdocindex/indexinfo"
 )
 
-const (
-	tmpl = `<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <title>{{.Name}}</title>
-  </head>
-  <body>
-    <h1>{{.Name}}</h1>
-
-    {{if and (eq (len .Directories) 0) (eq (len .Files) 0)}}
-    <p><b>No items</b></p>
-    {{end}}
-
-    {{if gt (len .Directories) 0}}
-    <h2>Directories</h2>
-    <ul>
-      {{range $i, $v := .Directories}}
-      <li><a href="{{$v}}/index.html">{{$v}}</a></li>
-      {{end}}
-    </ul>
-    {{end}}
-
-    {{if gt (len .Files) 0}}
-    <h2>Files</h2>
-    <ul>
-      {{range $i, $v := .Files}}
-      <li><a href="{{$v}}">{{$v}}</a></li>
-      {{end}}
-    </ul>
-    {{end}}
-  </body>
-</html>
-`
-)
-
 type IndexMaker struct {
 	path        string
 	directories []string

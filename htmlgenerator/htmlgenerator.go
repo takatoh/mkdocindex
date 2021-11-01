18b188b67d5263f1fa25fd53732b2a60883569bc
takatoh <takatoh.m@gmail.com>
2021-11-02 04:39:10 +0900

    htmlgenerator/htmlgenerator.go: Add infoMonolithic struct and related functions.

--- htmlgenerator/htmlgenerator.go
+++ htmlgenerator/htmlgenerator.go
@@ -49,3 +49,63 @@ func Generate(info *indexinfo.IndexInfo) {
 	w, _ := os.OpenFile(info.Path+"/index.html", os.O_WRONLY|os.O_CREATE, 0600)
 	t.ExecuteTemplate(w, "index", info)
 }
+
+const (
+	tmplMonolithic = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>{{.Title}}</title>
+  </head>
+  <body>
+    <header>
+      <h1>{{.Title}}</h1>
+    </header>
+    <main>
+    <div id="sidebar">
+      <h2>Table of contents</h2>
+      {{.TOC}}
+    </div>
+    <div id="container">
+      <div id="search-box">
+        <input type="text" id="search-word" placeholder="ページ内テキスト検索">
+        <input type="button" id="search-button" value="検索">
+        <input type="button" id="reset-button" value="リセット">
+      </div>
+      {{.Main}}
+    </div>
+    </main>
+  </body>
+</html>
+`
+)
+
+type infoMonolithic struct {
+	Title string
+	TOC   string
+	Main  string
+}
+
+func newInfoMonolithic(info *indexinfo.IndexInfoMonolithic) *infoMonolithic {
+	p := new(infoMonolithic)
+	p.Title = info.Name
+	p.TOC = generateTOC(info)
+	p.Main = generateMain(info)
+	return p
+}
+
+func GenerateMonolithic(info *indexinfo.IndexInfoMonolithic) {
+	infoM := newInfoMonolithic(info)
+	os.Remove(info.Path + "/index.html")
+	t, _ := template.New("index").Parse(tmplMonolithic)
+	w, _ := os.OpenFile(info.Path+"index.html", os.O_WRONLY|os.O_CREATE, 0600)
+	t.ExecuteTemplate(w, "index", infoM)
+}
+
+func generateTOC(info *indexinfo.IndexInfoMonolithic) string {
+	return "<h2>Table of contents</h2>"
+}
+
+func generateMain(info *indexinfo.IndexInfoMonolithic) string {
+	return "<h2>" + info.Name + "</h2"
+}

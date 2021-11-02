93e93877f4135518d01deab9ecbd666abfdf121f
takatoh <takatoh.m@gmail.com>
2021-11-02 09:02:47 +0900

    htmlgenerator/htmlgenerator.go: Apply style to monolithic html template.

--- htmlgenerator/htmlgenerator.go
+++ htmlgenerator/htmlgenerator.go
@@ -56,6 +56,7 @@ const (
   <head>
     <meta charset="utf-8">
     <title>{{.Title}}</title>
+      {{.Style|styletag}}
   </head>
   <body>
     <header>
@@ -64,7 +65,7 @@ const (
     <main>
     <div id="sidebar">
       <h2>Table of contents</h2>
-      {{.TOC}}
+      {{.TOC|safehtml}}
     </div>
     <div id="container">
       <div id="search-box">
@@ -72,11 +73,76 @@ const (
         <input type="button" id="search-button" value="検索">
         <input type="button" id="reset-button" value="リセット">
       </div>
-      {{.Main}}
+      {{.Main|safehtml}}
     </div>
     </main>
   </body>
 </html>
+`
+
+	styleMonolithic = `:root {
+  --header-height: 75px;
+  --sidebar-width: 25%;
+}
+
+* {
+  margin: 0;
+}
+
+header {
+  position: sticky;
+  width: 100%;
+  height: var(--header-height);
+  border-bottom: 1px solid grey;
+}
+
+header h1 {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 5%;
+}
+
+main {
+  display: flex;
+  min-height: calc(100vh - var(--header-height) - 1px);
+  width: 100%;
+  height: 100%;
+}
+
+main div#sidebar {
+  position: sticky;
+  width: var(--sidebar-width);
+  padding-top: 1.5em;
+  padding-left: 1.5em;
+  padding-right: 0.5em;
+  border-right: 1px solid grey;
+}
+
+main div#sidebar h2 {
+  margin-top: 0.2em;
+}
+
+main div#sidebar ul {
+  padding-left: 1.5em;
+  list-style: disc;
+}
+
+main div#sidebar li a {
+  color: black;
+  text-decoration: none;
+  word-break: break-all;
+}
+
+main div#container {
+  position: sticky;
+  padding: 1.5em;
+}
+
+.highlight {
+  font-weight: bold;
+  background-color: yellow;
+}
 `
 )
 
@@ -84,6 +150,7 @@ type infoMonolithic struct {
 	Title string
 	TOC   string
 	Main  string
+	Style string
 }
 
 func newInfoMonolithic(info *indexinfo.IndexInfoMonolithic) *infoMonolithic {
@@ -91,13 +158,20 @@ func newInfoMonolithic(info *indexinfo.IndexInfoMonolithic) *infoMonolithic {
 	p.Title = info.Name
 	p.TOC = genTOC(info)
 	p.Main = genMain(info)
+	p.Style = styleMonolithic
 	return p
 }
 
 func GenerateMonolithic(info *indexinfo.IndexInfoMonolithic) {
 	infoM := newInfoMonolithic(info)
+	funcMap := template.FuncMap{
+		"safehtml": func(text string) template.HTML { return template.HTML(text) },
+		"styletag": func(text string) template.HTML {
+			return template.HTML("<style>\n" + text + "</style>")
+		},
+	}
 	os.Remove(info.Path + "/index.html")
-	t, _ := template.New("index").Parse(tmplMonolithic)
+	t, _ := template.New("index").Funcs(funcMap).Parse(tmplMonolithic)
 	w, _ := os.OpenFile(info.Path+"/index.html", os.O_WRONLY|os.O_CREATE, 0600)
 	t.ExecuteTemplate(w, "index", infoM)
 }
